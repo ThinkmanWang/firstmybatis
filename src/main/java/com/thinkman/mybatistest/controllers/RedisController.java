af8761f3e8b5a1d0350dc7881643617f0c2ec669
Thinkman Wang <thinkman@function-hz.com>
2017-03-01 12:13:50 +0800

    update

---
+++ src/main/java/com/thinkman/mybatistest/controllers/RedisController.java
@@ -0,0 +1,28 @@
+package com.thinkman.mybatistest.controllers;
+
+import com.thinkman.mybatistest.service.MQConsumerService;
+import com.thinkman.mybatistest.service.MQProduceService;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RedisController {
+
+	@Autowired
+	private StringRedisTemplate mStringRedisTemplate;
+
+	private Logger logger = Logger.getLogger(UserController.class);
+
+
+	@RequestMapping("/redis.do")
+	public String redis() {
+		mStringRedisTemplate.opsForValue().set("aaa", "111");
+		logger.info(mStringRedisTemplate.opsForValue().get("aaa"));
+
+		return "success";
+	}
+
+}
